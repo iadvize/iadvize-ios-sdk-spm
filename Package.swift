3c95c9562897357a8bccf7e9ef6f527706b9130c
Simon Liotier <simon.liotier@iadvize.com>
2023-02-07 14:38:21 +0100

    Rename package

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let package = Package(name: "IAdvizeConversationSDK",
+let package = Package(name: "iAdvize",
                       platforms: [
                           .iOS("12.0")
                       ],
